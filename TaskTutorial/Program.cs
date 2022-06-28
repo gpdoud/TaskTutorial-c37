89da510c0bfb87d5859f2e879e659a60cd5d1522
Greg Doud <gpdoud@gmail.com>
2022-06-28 14:33:39 -0400

    final commit

---
+++ TaskTutorial/Program.cs
@@ -0,0 +1,34 @@
+﻿using TaskTutorial;
+
+Console.WriteLine("Ready!");
+
+Utility utility = new() { max = 5_000_000_000, fact = 10 };
+
+Task<int> tfactorial = Task<int>.Run(utility.Factorial);
+
+tfactorial.Wait();
+
+Console.WriteLine($"{utility.fact} factorial is {tfactorial.Result}");
+
+Task<ulong> tsum2max = Task<ulong>.Run(utility.SumToMax);
+
+//tsum2max.Wait();
+
+Console.WriteLine($"Sum number from 1 to {utility.max} is {tsum2max.Result}");
+
+
+/*
+int a = 7;
+int b = 3;
+
+//Task<int> t1 = Task<int>.Run( () => a - (a / b * b) );
+Task<int> t1 = new Task<int>(() => a - (a / b * b));
+
+a = 11;
+b = 3;
+
+t1.Start();
+t1.Wait();
+
+Console.WriteLine($"{a} % {b} = {t1.Result}");
+*/
---
+++ TaskTutorial/Utility.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskTutorial {
+    internal class Utility {
+
+        public ulong max { get; set; }
+        public int fact { get; set; }
+
+        public int Factorial() {
+            int answer = 1;
+            for(int i = 2; i <= fact; i++) {
+                answer *= i;
+            }
+            return answer;
+        }
+
+        public ulong SumToMax() {
+            ulong sum = 0;
+            for (ulong i = 1; i <= max; i++) {
+                sum += i;
+            }
+            return sum;
+        }
+    }
+}
